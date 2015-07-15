582a1624bd468c9ac032fe186a33ba963faddd21
Chris Taylor <chrispdtaylor@sky.com>
2015-07-15 15:45:16 +0000

    Added some jq functionality..

--- flask/static/js/shorthand.js
+++ flask/static/js/shorthand.js
@@ -1,3 +1,21 @@
 /**
  * Created by chris on 15/07/15.
  */
+
+$(document).ready(function() {
+
+        $('.toggle_button').click(function(){
+            var text = $(this).text();
+
+            if (text=="-"){
+                $(this).text('+').button("refresh");
+                $(this).next().hide(250);
+            }
+            if (text=="+"){
+                $(this).text('-').button("refresh");
+                $(this).next().show(250);
+            }
+
+        });
+
+});
