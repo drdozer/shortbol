2217aca67e37c623ca1b9dead53b8ec2ee86914a
Matthew Pocock <turingatemyhamster@gmail.com>
2016-06-23 11:44:41 +0100

    Exlpicitly testing import script with and without import pragma hook

--- shortbol/core/shared/src/test/scala/uk/co/turingatemyhamster/shortbol/EvalTestSuite.scala
+++ shortbol/core/shared/src/test/scala/uk/co/turingatemyhamster/shortbol/EvalTestSuite.scala
@@ -534,44 +534,65 @@ object EvalTestSuite extends TestSuite {
     }
 
     'import - {
-      val startCtxt = Ø.withPHooks(Import(Resolver.fromValues(
-        Url("http://xmlns.com/foaf/0.1/") -> parse(
-          """WithNameAge(name, age) => WithAge(age)
-            |  foaf:name = name
-            |
-            |WithAge(age) => foaf:person
-            |  foaf:age = age
-            |""".stripMargin
-        )
-      )).pHook)
+      * - {
+        val startCtxt = Ø
 
-      parse(
-        """@import <http://xmlns.com/foaf/0.1/>
-          |me : WithNameAge("matthew", 40)
-          |  foaf:knows = "caroline"
-          |""".stripMargin) in startCtxt evaluatesTo parse_instances(
-        """me : foaf:person
-          |  foaf:age = 40
-          |  foaf:name = "matthew"
-          |  foaf:knows = "caroline"
-          |""".stripMargin) in startCtxt.withConstructors (
-        parse_constructorDef(
-          """WithNameAge(name, age) => WithAge(age)
-            |  foaf:name = name
-            |""".stripMargin),
-        parse_constructorDef(
-          """WithAge(age) => foaf:person
-            |  foaf:age = age
-            |""".stripMargin)
-      ).withInstances(
-              parse_instances(
-                """me : foaf:person
-                  |  foaf:age = 40
-                  |  foaf:name = "matthew"
-                  |  foaf:knows = "caroline"
-                  |""".stripMargin).map { i => i.instanceExp} :_*).withPragmas(
-        Pragma("import", Seq(Url("http://xmlns.com/foaf/0.1/")))
-      )
+        parse(
+          """@import <http://xmlns.com/foaf/0.1/>
+            |me : WithNameAge("matthew", 40)
+            |  foaf:knows = "caroline"
+            |""".stripMargin) in startCtxt evaluatesTo parse_instances(
+          """me : WithNameAge("matthew", 40)
+            |  foaf:knows = "caroline"
+            |""".stripMargin) in startCtxt.withInstances(
+          parse_instances(
+            """me : WithNameAge("matthew", 40)
+              |  foaf:knows = "caroline"
+              |""".stripMargin).map { i => i.instanceExp} :_*).withPragmas(
+          Pragma("import", Seq(Url("http://xmlns.com/foaf/0.1/")))
+        )
+      }
+
+      * - {
+        val startCtxt = Ø.withPHooks(Import(Resolver.fromValues(
+          Url("http://xmlns.com/foaf/0.1/") -> parse(
+            """WithNameAge(name, age) => WithAge(age)
+              |  foaf:name = name
+              |
+              |WithAge(age) => foaf:person
+              |  foaf:age = age
+              |""".stripMargin
+          )
+        )).pHook)
+
+        parse(
+          """@import <http://xmlns.com/foaf/0.1/>
+            |me : WithNameAge("matthew", 40)
+            |  foaf:knows = "caroline"
+            |""".stripMargin) in startCtxt evaluatesTo parse_instances(
+          """me : foaf:person
+            |  foaf:age = 40
+            |  foaf:name = "matthew"
+            |  foaf:knows = "caroline"
+            |""".stripMargin) in startCtxt.withConstructors (
+          parse_constructorDef(
+            """WithNameAge(name, age) => WithAge(age)
+              |  foaf:name = name
+              |""".stripMargin),
+          parse_constructorDef(
+            """WithAge(age) => foaf:person
+              |  foaf:age = age
+              |""".stripMargin)
+        ).withInstances(
+                parse_instances(
+                  """me : foaf:person
+                    |  foaf:age = 40
+                    |  foaf:name = "matthew"
+                    |  foaf:knows = "caroline"
+                    |""".stripMargin).map { i => i.instanceExp} :_*).withPragmas(
+          Pragma("import", Seq(Url("http://xmlns.com/foaf/0.1/")))
+        )
+      }
     }
 
     'instance_reassignment - {
