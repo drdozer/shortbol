37304bed86684147cb122d8adcb36ef543e86483
drdozer <turingatemyhamster@gmail.com>
2016-07-02 23:09:26 +0100

    Fixed integration test now that constants are being correctly resolved.

--- shortbol/core/jvm/src/test/scala/uk/co/turingatemyhamster/shortbol/IntegrationTestSuite.scala
+++ shortbol/core/jvm/src/test/scala/uk/co/turingatemyhamster/shortbol/IntegrationTestSuite.scala
@@ -26,7 +26,7 @@ object IntegrationTestSuite extends TestSuite {
         parse_instances(
           """p : sbol:ComponentDefinition
             |  sbol:type = biopax:DnaRegion
-            |  sbol:role = sbol:Promoter""".stripMargin) in ⊥
+            |  sbol:role = <http://identifiers.org/so/SO:0000167>""".stripMargin) in ⊥
     }
 
     'dnaSequence_url - {
