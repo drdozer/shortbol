194fbf22bb795680f2ceb220631714cbbcb0e054
Matthew Pocock <turingatemyhamster@gmail.com>
2016-07-14 20:45:49 +0100

    Closes #22.

--- shortbol/server/js/src/main/scala/uk/co/turingatemyhamster/shortbol/client/Tutorial.scala
+++ shortbol/server/js/src/main/scala/uk/co/turingatemyhamster/shortbol/client/Tutorial.scala
@@ -17,6 +17,17 @@ object TutorialRoutes {
       Route("/addingSequences", () => TutorialPage(AddingSequences())) ::
       Nil
 
+  val nav = {
+    val rO = routes map Some.apply
+    (None :: rO) zip (rO ::: None :: Nil)
+  }
+
+  def before(at: Route): Option[Route] =
+    nav collectFirst { case (l, r) if r contains at => l } flatten
+
+  def after(at: Route): Option[Route] =
+    nav collectFirst { case (l, r) if l contains at => r } flatten
+
   val notFound = Route("/404", TutorialNotFound)
 }
 
@@ -31,29 +42,41 @@ trait TutorialContent {
 }
 
 case class TutorialPage(content: TutorialContent) extends Page {
+  import org.widok.bindings.HTML._
+  import TutorialUtils._
+
+  def prevNext(route: InstantiatedRoute) = {
+    val before = TutorialRoutes.before(route.route)
+    val after = TutorialRoutes.after(route.route)
+
+    val backTxt = "back" : View
+    val forwardTxt = "forward" : View
+
+    val back = before map (r => Anchor(backTxt).url(r())) getOrElse backTxt
+    val forward = after map (r => Anchor(forwardTxt).url(r())) getOrElse forwardTxt
+
+    Container.Generic(back, " ", forward).css("back_forward")
+  }
 
   override final def render(route: InstantiatedRoute) = Future.successful {
-    import org.widok.bindings.HTML._
-    import TutorialUtils._
     Container.Generic(
       Header(
         Image("images/logo.png").css("logo"),
         Heading.Level1(v"$shortbol Tutorial")
       ),
       Container.Generic(
-        Container.Generic(
-          Navigation(
-            List.Unordered(
-              TutorialRoutes.routes.map { case r@Route(path, page) =>
-                page() match {
-                  case TutorialPage(tc) =>
-                    List.Item(Anchor(tc.navigationEntry).url(r()))
-                }
-              } :_*
-            )
-          ),
-          content.render(route)
-        )
+        Navigation(
+          List.Unordered(
+            TutorialRoutes.routes.map { case r@Route(path, page) =>
+              page() match {
+                case TutorialPage(tc) =>
+                  List.Item(Anchor(tc.navigationEntry).url(r()))
+              }
+            } :_*
+          )
+        ),
+        content.render(route),
+        prevNext(route)
       ).css("tutorialContent"),
       Footer(
         List.Unordered(
