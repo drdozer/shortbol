e6b88574762b04b5b2c9f6dac1999c77bed02ecb
Matthew Pocock <turingatemyhamster@gmail.com>
2016-11-15 17:47:44 +0000

    Fixup on the sandbox. Removing obosolete command-line application.

--- shortbol/server/js/src/main/scala/uk/co/turingatemyhamster/shortbol/client/Sandbox.scala
+++ shortbol/server/js/src/main/scala/uk/co/turingatemyhamster/shortbol/client/Sandbox.scala
@@ -8,11 +8,13 @@ import org.widok._
 import org.widok.html._
 import pl.metastack.metarx.Buffer
 import uk.co.turingatemyhamster.datatree
+import uk.co.turingatemyhamster.datatree.ast.AstDatatree
 import uk.co.turingatemyhamster.datatree.io.RdfIo
 import uk.co.turingatemyhamster.shortbol.shorthandAst.SBFile
 import uk.co.turingatemyhamster.shortbol.ops.Eval.EvalOps
 import uk.co.turingatemyhamster.shortbol.ops.ShortbolParser.POps
-import uk.co.turingatemyhamster.shortbol.ops.{Exporter, LogMessage, PrettyPrinter, ShortbolParser}
+import uk.co.turingatemyhamster.shortbol.ops.rewriteRule.{RepairComponents, RepairIdentities}
+import uk.co.turingatemyhamster.shortbol.ops._
 
 import scala.scalajs.js
 
@@ -29,6 +31,7 @@ object Sandbox extends PageApplication {
 
   override def view() = div(
     h1("Shortbol sandpit"),
+    h2("Editor"),
     div(
       """@import stdlib:sbol
         |
@@ -45,8 +48,13 @@ object Sandbox extends PageApplication {
         |  name = "Promoter 1"
         |  description = "The first promoter"
         |""".stripMargin).id("shortbol_editor").css("shortbol_ace"),
+    h2("Template expansion"),
     div().id("longbol").css("shortbol_ace"),
+    h2("Fixed up"),
+    div().id("fixedbol").css("shortbol_ace"),
+    h2("SBOl RDF"),
     div().id("xml-rdf").css("shortbol_ace"),
+    h2("Log messages"),
     div(logMsgs map { m => div(m.pretty).css(m.level.pretty) }).id("log")
     )
 
@@ -63,10 +71,15 @@ object Sandbox extends PageApplication {
     editor.getSession().setMode("ace/mode/sbolshorthand2")
     println("Set mode")
 
-    val result = ace.edit("longbol")
-    result.setTheme("ace/theme/solarized_light")
-    result.getSession().setMode("ace/mode/sbolshorthand2")
-    result.setReadOnly(true)
+    val longbol = ace.edit("longbol")
+    longbol.setTheme("ace/theme/solarized_light")
+    longbol.getSession().setMode("ace/mode/sbolshorthand2")
+    longbol.setReadOnly(true)
+
+    val fixedbol = ace.edit("fixedbol")
+    fixedbol.setTheme("ace/theme/solarized_light")
+    fixedbol.getSession().setMode("ace/mode/sbolshorthand2")
+    fixedbol.setReadOnly(true)
 
     val xmlRdf = js.Dynamic.global.ace.edit("xml-rdf")
     xmlRdf.setTheme("ace/theme/solarized_light")
@@ -81,32 +94,36 @@ object Sandbox extends PageApplication {
 
       ShortbolParser.SBFile.withPositions(shorthandAst.LocalName("shortbol_editor"), txt) match {
         case s: Success[SBFile] =>
-          val (c, v) = s.value.eval.run(Fixture.configuredContext)
+          val (c1, v1) = s.value.eval.run(Fixture.configuredContext)
 
 //          println("Setting log messages")
-          logMsgs.clear()
-          logMsgs ++= c.logms
 
-          val sw = new StringWriter()
-          PrettyPrinter(sw)(v)
-          sw.append("\n")
-//          println("Evaluated to:")
-//          println(ast.SBFile(v))
-//          println("Pretty printed as:")
-//          println(sw)
+          def stringify(sf: longhandAst.SBFile) = {
+            val sw = new StringWriter()
+            PrettyPrinter(sw)(sf)
+            sw.append("\n")
+            sw.toString
+            sw
+          }
 
-          result.getSession().setValue(sw.toString)
+          longbol.getSession().setValue(stringify(v1).toString)
+
+          val (c2, v2) = RewriteRule.rewrite(
+            RepairComponents.repairAll andThen RepairIdentities.repairAll, v1).run(c1)
+
+          fixedbol.getSession().setValue(stringify(v2).toString)
 
 //          println("Exporting to datatree")
-          val doc = Exporter[datatree.ast.AstDatatree](c).apply(v)
+          val doc = Exporter[AstDatatree](c2).apply(v2)
 //          println("Exporting to xml-rdf")
-          val rdfIo = RdfIo.rdfIo[datatree.ast.AstDatatree]
-          val xml = RdfIo.write[datatree.ast.AstDatatree](doc)
+          val rdfIo = RdfIo.rdfIo[AstDatatree]
+          val xml = RdfIo.write[AstDatatree](doc)
           val xmlText = xml.render(2)
 
           xmlRdf.getSession().setValue(xmlText)
 
-//          rdf.
+          logMsgs.clear()
+          logMsgs ++= c2.logms
       }
     }
   }
